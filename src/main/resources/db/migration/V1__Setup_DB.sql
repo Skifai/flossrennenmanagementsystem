5227e1ec0ec49bb23098d7df6cfeb576110d5bae
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-12-26 21:31:31 +0100

    Change DB Schema

---
+++ src/main/resources/db/migration/V1__Setup_DB.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS benutzer
+(
+    id            BIGSERIAL PRIMARY KEY,
+    vorname       VARCHAR(100)        NOT NULL,
+    nachname      VARCHAR(100)        NOT NULL,
+    telefonnummer VARCHAR(15) UNIQUE  NOT NULL,
+    email         VARCHAR(254) UNIQUE NOT NULL,
+    passwordhash  VARCHAR(100)        NOT NULL,
+    rolle         VARCHAR(50)         NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ressort
+(
+    id             BIGSERIAL PRIMARY KEY,
+    name           VARCHAR(100) UNIQUE NOT NULL,
+    beschreibung   VARCHAR(300),
+    zustaendigkeit VARCHAR(300),
+    verantwortlich BIGINT REFERENCES benutzer (id)
+);
+
+CREATE TABLE IF NOT EXISTS helfer
+(
+    id            BIGSERIAL PRIMARY KEY,
+    vorname       VARCHAR(100)        NOT NULL,
+    nachname      VARCHAR(100)        NOT NULL,
+    email         VARCHAR(254) UNIQUE NOT NULL,
+    telefonnummer VARCHAR(15) UNIQUE  NOT NULL,
+    ressort       BIGINT              NOT NULL REFERENCES ressort (id)
+);
