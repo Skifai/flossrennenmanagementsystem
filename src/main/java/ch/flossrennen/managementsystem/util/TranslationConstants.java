fb67ea2b57a183100eca195c5b6c1f8c5aa79cca
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-12-26 21:49:54 +0100

    Add more Translations

--- src/main/java/ch/flossrennen/managementsystem/util/TranslationConstants.java
+++ src/main/java/ch/flossrennen/managementsystem/util/TranslationConstants.java
@@ -2,9 +2,12 @@ package ch.flossrennen.managementsystem.util;
 
 public interface TranslationConstants {
 
+    // Global
     String TITEL = "titel";
     String SUBTITEL = "subtitel";
     String LOGO_DESC = "logodesc";
+
+    // Login
     String LOGIN_TITLE = "logintitel";
     String USERNAME = "username";
     String PASSWORD = "password";
@@ -13,9 +16,24 @@ public interface TranslationConstants {
     String LOGIN_ERROR_MESSAGE = "loginerrormessage";
 
     // Entity Helfer
-    String ID = "helfer.id";
-    String VORNAME = "helfer.vorname";
-    String NACHNAME = "helfer.nachname";
-    String EMAIL = "helfer.email";
-    String TELEFONNUMMER = "helfer.telefonnummer";
+    String HELFER_ID = "helfer.id";
+    String HELFER_VORNAME = "helfer.vorname";
+    String HELFER_NACHNAME = "helfer.nachname";
+    String HELFER_EMAIL = "helfer.email";
+    String HELFER_TELEFONNUMMER = "helfer.telefonnummer";
+    String HELFER_RESSORT = "helfer.ressort";
+
+    // Entity Benutzer
+    String BENUTZER_VORNAME = "benutzer.vorname";
+    String BENUTZER_NACHNAME = "benutzer.nachname";
+    String BENUTZER_TELEFONNUMMER = "benutzer.telefonnummer";
+    String BENUTZER_EMAIL = "benutzer.email";
+    String BENUTZER_PASSWORDHASH = "benutzer.passwordhash";
+    String BENUTZER_ROLLE = "benutzer.rolle";
+
+    // Entity Ressort
+    String RESSORT_NAME = "ressort.name";
+    String RESSORT_BESCHREIBUNG = "ressort.beschreibung";
+    String RESSORT_ZUSTAENDIGKEIT = "ressort.zustaendigkeit";
+    String RESSORT_VERANTWORTLICH = "ressort.verantwortlich";
 }
