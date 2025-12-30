ca40cf51574694c5b94854e3d62e15dbfb321513
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-12-30 17:39:39 +0100

    Style und Translation Konstanten implementiert

--- src/main/java/ch/flossrennen/managementsystem/FlossrennenManagementSystemApplication.java
+++ src/main/java/ch/flossrennen/managementsystem/FlossrennenManagementSystemApplication.java
@@ -1,12 +1,13 @@
 package ch.flossrennen.managementsystem;
 
+import ch.flossrennen.managementsystem.view.ViewStyles;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Theme("flossrennen-theme")
+@Theme(ViewStyles.THEME_NAME)
 public class FlossrennenManagementSystemApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
