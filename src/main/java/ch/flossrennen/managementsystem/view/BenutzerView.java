4cbe72f3cd70e93b6a8b74da9ed810a330fa6b20
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-12-30 17:39:11 +0100

    Style und Translation Konstanten implementiert

--- src/main/java/ch/flossrennen/managementsystem/view/BenutzerView.java
+++ src/main/java/ch/flossrennen/managementsystem/view/BenutzerView.java
@@ -3,6 +3,7 @@ package ch.flossrennen.managementsystem.view;
 import ch.flossrennen.managementsystem.dataaccess.dto.BenutzerDTO;
 import ch.flossrennen.managementsystem.dataaccess.dto.BenutzerDTOProperties;
 import ch.flossrennen.managementsystem.service.DTOService;
+import ch.flossrennen.managementsystem.util.TranslationConstants;
 import ch.flossrennen.managementsystem.view.editor.AbstractEditorView;
 import ch.flossrennen.managementsystem.view.editor.BenutzerEditor;
 import com.vaadin.flow.router.Route;
@@ -14,7 +15,7 @@ import org.jspecify.annotations.NonNull;
 public class BenutzerView extends AbstractContentBaseView<BenutzerDTO, BenutzerDTOProperties> {
 
     public BenutzerView(DTOService<BenutzerDTO> benutzerDTOService) {
-        super(benutzerDTOService, BenutzerDTO.class, BenutzerDTOProperties.values(), "Benutzerverwaltung", "Neuer Benutzer", BenutzerDTO::createEmptyDTO);
+        super(benutzerDTOService, BenutzerDTO.class, BenutzerDTOProperties.values(), TranslationConstants.BENUTZER_TITLE, TranslationConstants.BENUTZER_NEW, BenutzerDTO::createEmptyDTO);
     }
 
     @Override
