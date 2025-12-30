4603878a68f94d13827ac5f23dc0786a7095f21d
Maikel Baumann <maikel.baumann.gmx.ch@zbw-online.ch>
2025-12-30 16:59:33 +0100

    Styles Konstanten Interface hinzugefügt

---
+++ src/main/java/ch/flossrennen/managementsystem/view/ViewStyles.java
@@ -0,0 +1,26 @@
+package ch.flossrennen.managementsystem.view;
+
+public interface ViewStyles {
+
+    // Themes
+    String THEME_NAME = "flossrennen-theme";
+
+    // HeaderBar
+    String APP_TITEL = "AppTitel";
+    String APP_DRAWER_TOGGLE = "AppDrawerToggle";
+
+    // NavBar
+    String APP_NAVBAR_LAYOUT = "AppNavbarLayout";
+    String MAIN_VIEW_SIDE_NAV = "MainViewSideNav";
+
+    // MainViewContent
+    String MAIN_VIEW_CONTENT = "MainViewContent";
+    String MAIN_VIEW_CONTENT_ELEMENTS = "MainViewContentElements";
+    String EDITOR_VIEW = "EditorView";
+
+    // Components
+    String FIELD_FILTER = "FieldFilter";
+    String DATA_GRID = "DataGrid";
+    String EDITOR_FIELD = "EditorField";
+    String BUTTON = "Button";
+}
